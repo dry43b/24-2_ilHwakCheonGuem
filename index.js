4a0d7613102fffe1bd2853517324f395206f1bac
Heo Jaewon <dry43b@gmail.com>
2024-12-02 09:36:27 +0900

    index.js 추가

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3001;
+
+// 라우트 설정
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+// 서버 시작
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
